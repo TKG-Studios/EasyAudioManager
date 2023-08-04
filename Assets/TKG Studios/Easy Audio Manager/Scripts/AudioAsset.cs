e09c4587981d85fecf2923e1030898ce218e979f
Christopher Guyton <christopherguyton87@gmail.com>
2023-08-04 12:29:12 -0700

    Added subtle animations

---
+++ Assets/TKG Studios/Easy Audio Manager/Scripts/AudioAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class AudioAsset
+{
+    public string name;
+    [HideInInspector] public float volume = 1f;
+    public AudioClip[] clip;
+    public AudioMixerGroup mixerGroup;
+
+}
